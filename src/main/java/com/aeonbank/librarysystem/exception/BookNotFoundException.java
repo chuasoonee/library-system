3676c91cf8f406b52905ba1555f173a46494e107
Soon Ee <soonee.chua@mecan.com>
2025-05-25 18:40:16 +0800

    Fixed mispelled message

--- src/main/java/com/aeonbank/librarysystem/exception/BookNotFoundException.java
+++ src/main/java/com/aeonbank/librarysystem/exception/BookNotFoundException.java
@@ -4,7 +4,7 @@ public class BookNotFoundException extends RuntimeException {
 
 	private static final long serialVersionUID = 7735025657161122995L;
 
-	public BookNotFoundException(Long borrowerId) {
-		super("Borrower with ID: " + borrowerId + " not found");
+	public BookNotFoundException(Long bookId) {
+		super("Book with ID: " + bookId + " not found");
 	}
 }
