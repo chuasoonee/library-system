8f172f71bd70c39e2b0eb25ae46a6847772d11fe
Soon Ee <soonee.chua@mecan.com>
2025-05-25 18:40:38 +0800

    Updated message

--- src/main/java/com/aeonbank/librarysystem/exception/InvalidBookException.java
+++ src/main/java/com/aeonbank/librarysystem/exception/InvalidBookException.java
@@ -5,6 +5,6 @@ public class InvalidBookException extends RuntimeException {
 	private static final long serialVersionUID = 7451070238006924335L;
 	
 	public InvalidBookException(String isbn, String title, String author) {
-		super("Book with ISBN: " + isbn + " already exists with different title or author");
+		super("Book with ISBN: " + isbn + " already exists with different title: " + title + " or author: " + author);
 	}
 }
